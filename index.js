a5e124b07907b43ba219f0dbbebb41efc9a73173
Shahd-ngm <143643590+Shahd-ngm@users.noreply.github.com>
2023-11-11 00:05:17 +0400

    c

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+
+// // audio to play as soon as html loads
+// window.onload = function() {
+//     document.getElementById("my_audio").play();
+// }
+
+// function myFunction() { 
+//  document.getElementById("my_audio").play();
+//   document.getElementById("my_audio").load();
+// } 
+
+
+
 startButton = document.getElementById('start-button');
 
 console.log(startButton.textContent)
 startButton.addEventListener('click', () => {
     window.location.href = "./campus.html"
 });
+
+audioElement.play();
